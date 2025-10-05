ef75d24aff9a40e25b211fcf8a3b9918a00250bf
Mhd-Hsyn <syd.mhd.hsyn@gmail.com>
2025-10-05 21:39:24 +0500

    use recommended asynccontextmanager for on_startup and on_shutdown

--- app/main.py
+++ app/main.py
@@ -1,12 +1,23 @@
 from fastapi import FastAPI
+from contextlib import asynccontextmanager
 from app.config.database import init_db
 from app.auth.routes import auth_router
+import uvicorn
 
 app = FastAPI()
 
 app.include_router(auth_router, prefix="/api/auth", tags=["Auth"])
 
 
-@app.on_event("startup")
-async def on_startup():
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # --- Startup ---
     await init_db()
+    yield
+    # --- Shutdown ---
+    print("App shutting down...")
+
+if __name__ == "__main__":
+    uvicorn.run("app.main:app", host="0.0.0.0", port=8000, reload=True)
+
+
