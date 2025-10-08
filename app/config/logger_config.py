2c839bf9401802af2e5a579a51f130f7a68c698e
Mhd-Hsyn <syd.mhd.hsyn@gmail.com>
2025-10-08 20:55:34 +0500

    add logger for helper

--- app/config/logger_config.py
+++ app/config/logger_config.py
@@ -15,6 +15,7 @@ logger.remove()
 logger.add(
     sys.stdout,
     colorize=True,
+    serialize=True,
     format="<green>{time:YYYY-MM-DD HH:mm:ss}</green> | "
            "<level>{level: <8}</level> | "
            "<cyan>{name}</cyan>:<cyan>{function}</cyan>:<cyan>{line}</cyan> - "
